67a8d04edf770b1b894de7332c17edeefd4accda
muthiakhrnsa3 <127019755+muthiakhrnsa3@users.noreply.github.com>
2023-03-05 19:38:55 +0700

    halo

---
+++ tpmod3_1302213062/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tpmod3_1302213062
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
